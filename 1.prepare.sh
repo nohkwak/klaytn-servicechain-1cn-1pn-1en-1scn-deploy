4c90d895916c5206a577c26e28d7248296e120b1
Nohyun Nehemiah Kwak <kwakmark@gmail.com>
2022-01-24 17:28:22 +0900

    Delete unused script

--- 1.prepare.sh
+++ 1.prepare.sh
@@ -3,8 +3,9 @@ CHAIN_ID=1000
 SCHAIN_ID=2000
 NETWORK_ID=203
 NUM_CNS=1
-PRIVATE_KEY=492cf9f1e9574e7f9f3e914dcf18117280cf238776ca715da47996ac1b36b98b
-ADDRESS=0x09D790C4717dF9F71382b243f2A0B1DE34a36120
+NUM_PNS=1
+NUM_ENS=1
+NUM_SCN=1
 
 # Download klaytn docker image.
 docker pull klaytn/klaytn:latest
@@ -12,14 +13,6 @@ docker pull klaytn/klaytn:latest
 # Generate docker-compose.yml for docker-compose.
 docker run --rm -v ${PWD}:/homi-output klaytn/klaytn homi setup -o /homi-output \
 	--docker-image-id klaytn/klaytn:latest \
-	--chainID $CHAIN_ID --cn-num $NUM_CNS  --pn-num 1 --en-num 1 \
-	--servicechain --serviceChainID $SCHAIN_ID --scn-num 1 \
+	--chainID $CHAIN_ID --cn-num $NUM_CNS  --pn-num $NUM_PNS --en-num $NUM_ENS \
+	--servicechain --serviceChainID $SCHAIN_ID --scn-num $NUM_SCN \
 	--network-id $NETWORK_ID docker
-
-# Update private key if necessary
-./update_genesis_account.sh
-
-# Add the whale accojunt to the node wallet.
-PK=`grep "nodekeyhex" docker-compose.yml | sed "s/.*nodekeyhex \(.*\)\".*/\1/"`
-REWARDBASE=`grep "REWARDBASE" docker-compose.yml | sed "s/.*REWARDBASE=\(.*\)\'.*/\1/"`
-#./add_import_key.py docker-compose.yml $PK $REWARDBASE
